7bf8c95a253b4d21ff7fbc4d9ea40a468036a81b
aafanasi <Anton.Afanasiev@genesys.com>
2021-11-22 12:50:51 -0400

    remove KVault dependency that contains androidx.

--- transport/src/iosMain/kotlin/com/genesys/cloud/messenger/transport/util/TokenStoreImpl.kt
+++ transport/src/iosMain/kotlin/com/genesys/cloud/messenger/transport/util/TokenStoreImpl.kt
@@ -1,11 +1,10 @@
 package com.genesys.cloud.messenger.transport.util
 
-import com.liftric.kvault.KVault
 import platform.Foundation.NSUUID
 
 internal class TokenStoreImpl(storeKey: String) : TokenStore {
 
-    private val store = KVault(storeKey)
+    private val store = Vault(storeKey)
     override val token: String
         get() = store.string(TOKEN_KEY) ?: NSUUID().UUIDString().also {
             store.set(TOKEN_KEY, it)
