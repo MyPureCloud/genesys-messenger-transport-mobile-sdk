54908ef910d5b0b4dd2f5b7d7b57c6e86ead4de4
aafanasi <Anton.Afanasiev@genesys.com>
2022-01-06 11:00:50 -0400

    Change MessageDispatcher.kt visibility modifier.

--- transport/src/androidTest/kotlin/com/genesys/cloud/messenger/transport/util/MessageDispatcherTest.kt
+++ transport/src/androidTest/kotlin/com/genesys/cloud/messenger/transport/util/MessageDispatcherTest.kt
@@ -6,7 +6,6 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
 
-
 class MessageDispatcherTest {
     private val mockListener = mockk<MessageListener>(relaxed = true)
     private val subject = MessageDispatcher(mockListener)
