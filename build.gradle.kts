5b92da670f30ec1b598a3129e3daa6f3bf379b2d
Chris Rumpf <chris.rumpf@genesys.com>
2022-02-04 15:53:33 -0500

    build: update gradle plugin to stable version 7.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-        classpath("com.android.tools.build:gradle:7.1.0-alpha05")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("org.jmailen.gradle:kotlinter-gradle:3.4.0")
         classpath("com.vanniktech:gradle-android-junit-jacoco-plugin:0.16.0")
     }
