27086bbe2000d348cba65cbf332e33f928a8093a
Chris Rumpf <chris.rumpf@genesys.com>
2022-04-07 15:46:23 -0400

    chore: update tools to support Xcode 13 and Apple Silicon
    docs: update README.md with Xcode and CocoaPods notes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    val compose_version by extra("1.0.3")
+    val compose_version by extra("1.1.1")
 
     repositories {
         gradlePluginPortal()
@@ -7,8 +7,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.1.2")
         classpath("org.jmailen.gradle:kotlinter-gradle:3.4.0")
         classpath("com.vanniktech:gradle-android-junit-jacoco-plugin:0.16.0")
     }
