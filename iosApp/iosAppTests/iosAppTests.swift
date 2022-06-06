083b73b26ab2e6ac06da81a408f2c689a08f737f
Morehouse, Matthew <Matthew.Morehouse@inin.com>
2022-06-06 10:09:04 -0400

    GMDX-481: New test for attaching and sending an image.

--- iosApp/iosAppTests/iosAppTests.swift
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -31,6 +31,45 @@ class iosAppTests: XCTestCase {
         super.tearDown()
     }
 
+    func testAttachments() {
+        // Setup the session. Send a message to start the conversation.
+        // Setup the session. Send a message.
+        guard let contentController = contentController else {
+            XCTFail("Faild to setup the content controller.")
+            return
+        }
+
+        contentController.connect()
+        contentController.configureSession()
+        contentController.sendMessage(text: "Testing from E2E test.")
+
+        // Use the public API to answer the new Messenger conversation.
+        // Send a message from that agent and make sure we receive it.
+        guard let conversationInfo = ApiHelper.shared.answerNewConversation() else {
+            XCTFail("The message we sent may not have connected to an agent.")
+            return
+        }
+
+        // Send an attachment from the transport SDK. Ensure it's received
+        guard let image = TestConfig.shared.pullTestPng(), let data = image.pngData() as NSData? else {
+            XCTFail("Failed to pull the test image.")
+            return
+        }
+        let swiftByteArray: [UInt8] = data.toByteArray()
+        let intArray : [Int8] = swiftByteArray
+            .map { Int8(bitPattern: $0) }
+        let kotlinByteArray: KotlinByteArray = KotlinByteArray.init(size: Int32(swiftByteArray.count))
+        for (index, element) in intArray.enumerated() {
+            kotlinByteArray.set(index: Int32(index), value: element)
+        }
+        contentController.attachImage(kotlinByteArray: kotlinByteArray)
+        contentController.sendUploadedImage()
+
+        // Disconnect the conversation for the agent and disconnect the session.
+        ApiHelper.shared.sendConnectOrDisconnect(conversationInfo: conversationInfo, connecting: false, wrapup: true)
+        contentController.disconnect()
+    }
+
     func testSendAndReceiveMessage() {
         // Setup the session. Send a message.
         guard let contentController = contentController else {
@@ -65,6 +104,7 @@ class TestContentController: MessengerHandler {
 
     var testExpectation: XCTestExpectation? = nil
     var receivedMessageText: String? = nil
+    var receivedDownloadUrl: String? = nil
 
     override func connect() {
         testExpectation = XCTestExpectation(description: "Wait for Connection.")
@@ -92,8 +132,25 @@ class TestContentController: MessengerHandler {
         verifyReceivedMessage(expectedMessage: text)
     }
 
+    override func attachImage(kotlinByteArray: KotlinByteArray) {
+        testExpectation = XCTestExpectation(description: "Wait for image to attach successfully.")
+        super.attachImage(kotlinByteArray: kotlinByteArray)
+        waitForExpectation()
+    }
+
+    func sendUploadedImage() {
+        testExpectation = XCTestExpectation(description: "Wait for the uploaded image url to send.")
+        guard let receivedDownloadUrl = receivedDownloadUrl else {
+            XCTFail("There was no download URL received.")
+            return
+        }
+        super.sendMessage(text: receivedDownloadUrl)
+        waitForExpectation()
+        verifyReceivedMessage(expectedMessage: receivedDownloadUrl)
+    }
+
     func waitForExpectation() {
-        let result = XCTWaiter().wait(for: [testExpectation!], timeout: 30)
+        let result = XCTWaiter().wait(for: [testExpectation!], timeout: 60)
         XCTAssertEqual(result, .completed, "Expectation never fullfilled: \(testExpectation?.description ?? "No description.")")
     }
 
@@ -139,7 +196,11 @@ class TestContentController: MessengerHandler {
                 self?.testExpectation?.fulfill()
             case let attachmentUpdated as MessageEvent.AttachmentUpdated:
                 print("Attachment Updated: <\(attachmentUpdated.attachment.description)>")
-                self?.testExpectation?.fulfill()
+                // Only finish the wait when the attachment has finished uploading.
+                if let uploadedAttachment = attachmentUpdated.attachment.state as? Attachment.StateUploaded {
+                    self?.receivedDownloadUrl = uploadedAttachment.downloadUrl
+                    self?.testExpectation?.fulfill()
+                }
             case let history as MessageEvent.HistoryFetched:
                 print("start of conversation: <\(history.startOfConversation.description)>, messages: <\(history.messages.description)>")
                 self?.testExpectation?.fulfill()
