daca12ee3a8797c70ee9f6e7531567d0c021e0e0
Chris Rumpf <chris.rumpf@genesys.com>
2022-06-15 16:20:55 -0400

    factoring UI concerns out of MessengerHandler
    * factoring UI concerns out of MessengerHandler
    * some UI updates in ContentViewController
    * added pre-build action to make sure a config.json file exists or else Xcode goes insane

--- iosApp/iosApp/MessengerHandler.swift
+++ iosApp/iosApp/MessengerHandler.swift
@@ -3,7 +3,7 @@
 //  iosApp
 //
 //  Created by Morehouse, Matthew on 6/2/22.
-//  Copyright © 2022 orgName. All rights reserved.
+//  Copyright © 2022 Genesys. All rights reserved.
 //
 
 import Foundation
@@ -12,11 +12,13 @@ import UIKit
 
 class MessengerHandler {
 
-    let client: MessagingClient
     private let deployment: Deployment
-    let config: Configuration
-    var outputLabel: UILabel? = nil
-
+    private let config: Configuration
+    let client: MessagingClient
+    
+    public var onStateChange: ((MessagingClientState) -> Void)?
+    public var onMessageEvent: ((MessageEvent) -> Void)?
+    
     init(deployment: Deployment) {
         self.deployment = deployment
         self.config = Configuration(deploymentId: deployment.deploymentId!,
@@ -24,139 +26,94 @@ class MessengerHandler {
                                     tokenStoreKey: "com.genesys.cloud.messenger",
                                     logging: true)
         self.client = MobileMessenger().createMessagingClient(configuration: self.config)
-    }
-
-    func setOutputLabel(outputLabel: UILabel) {
-        self.outputLabel = outputLabel
-    }
-
-    func setupSocketListeners() {
+        
         client.stateListener = { [weak self] state in
-            switch state {
-            case _ as MessagingClientState.Connecting:
-                print("connecting state")
-                self?.outputLabel?.text = "<connecting>"
-            case _ as MessagingClientState.Connected:
-                print("connected")
-                self?.outputLabel?.text = "<connected>"
-            case let configured as MessagingClientState.Configured:
-                print("Socket <configured>. connected: <\(configured.connected.description)> , newSession: <\(configured.newSession?.description ?? "nill")>")
-                self?.outputLabel?.text = "Socket <configured>. connected: <\(configured.connected.description)> , newSession: <\(configured.newSession?.description ?? "nill")>"
-            case let closing as MessagingClientState.Closing:
-                print("Socket <closing>. reason: <\(closing.reason.description)> , code: <\(closing.code.description)>")
-                self?.outputLabel?.text = "Socket <closing>. reason: <\(closing.reason.description)> , code: <\(closing.code.description))>"
-            case let closed as MessagingClientState.Closed:
-                print("Socket <closed>. reason: <\(closed.reason.description)> , code: <\(closed.code.description)>")
-                self?.outputLabel?.text = "Socket <closed>. reason: <\(closed.reason.description)> , code: <\(closed.code.description)>"
-            case let error as MessagingClientState.Error:
-                print("Socket <error>. code: <\(error.code.description)> , message: <\(error.message ?? "No message")>")
-                self?.outputLabel?.text = "Socket <error>. code: <\(error.code.description)> , message: <\(error.message ?? "No message")>"
-            default:
-                print("Unexpected stateListener state: \(state)")
-            }
+            print("State Change: \(state)")
+            self?.onStateChange?(state)
         }
-    }
-
-    func setupMessageListener() {
         client.messageListener = { [weak self] event in
-            switch event {
-            case let messageInserted as MessageEvent.MessageInserted:
-                self?.outputLabel?.text = "Message Inserted: <\(messageInserted.message.description)>"
-            case let messageUpdated as MessageEvent.MessageUpdated:
-                self?.outputLabel?.text = "Message Updated: <\(messageUpdated.message.description)>"
-            case let attachmentUpdated as MessageEvent.AttachmentUpdated:
-                self?.outputLabel?.text = "Attachment Updated: <\(attachmentUpdated.attachment.description)>"
-            case let history as MessageEvent.HistoryFetched:
-                self?.outputLabel?.text = "start of conversation: <\(history.startOfConversation.description)>, messages: <\(history.messages.description)> "
-            default:
-                print("Unexpected messageListener event: \(event)")
-            }
+            print("Message Event: \(event)")
+            self?.onMessageEvent?(event)
         }
     }
 
-    func configureSession() {
-        do {
-            try client.configureSession()
-        } catch {
-            print("Failed to configure session. \(error.localizedDescription)")
-            outputLabel?.text = "<\(error.localizedDescription)>"
-        }
-    }
-
-    func connect() {
+    func connect() throws {
         do {
             try client.connect()
         } catch {
-            print(error)
-            outputLabel?.text = "<\(error.localizedDescription)>"
+            print("connect() failed. \(error.localizedDescription)")
+            throw error
+        }
+    }
+    
+    func configureSession() throws {
+        do {
+            try client.configureSession()
+        } catch {
+            print("configureSession() failed. \(error.localizedDescription)")
+            throw error
         }
     }
 
-    func disconnect() {
+    func disconnect() throws {
         do {
             try client.disconnect()
         } catch {
-            print(error)
-            outputLabel?.text = "<\(error.localizedDescription)>"
+            print("disconnect() failed. \(error.localizedDescription)")
+            throw error
         }
     }
 
-    func sendMessage(text: String) {
+    func sendMessage(text: String) throws {
         do {
             try client.sendMessage(text: text.trimmingCharacters(in: .whitespaces))
         } catch {
-            print(error)
-            outputLabel?.text = "<\(error.localizedDescription)>"
+            print("sendMessage(text:) failed. \(error.localizedDescription)")
+            throw error
         }
     }
 
-    func fetchNextPage() {
+    func fetchNextPage(completion: ((Error?) -> Void)? = nil) {
         client.fetchNextPage() {_, error in
-            if let error = error {
-                self.outputLabel?.text = "<\(error.localizedDescription)>"
-                return
-            }
+            completion?(error)
         }
     }
 
-    func sendHealthCheck() {
+    func sendHealthCheck() throws {
         do {
             try client.sendHealthCheck()
         } catch {
-            print(error)
-            outputLabel?.text = "<\(error.localizedDescription)>"
+            print("sendHealthCheck() failed. \(error.localizedDescription)")
+            throw error
         }
     }
 
-    func attachImage(kotlinByteArray: KotlinByteArray) {
+    func attachImage(kotlinByteArray: KotlinByteArray) throws {
         do {
             try client.attach(byteArray: kotlinByteArray, fileName: "image.png", uploadProgress: { progress in
                 print("Attachment upload progress: \(progress)")
             })
         } catch {
-            print(error)
-            outputLabel?.text = "<\(error.localizedDescription)>"
+            print("attachImage(kotlinByteArray:) failed. \(error.localizedDescription)")
+            throw error
         }
     }
 
-    func detachImage(attachId: String) {
+    func detachImage(attachId: String) throws {
         do {
             try client.detach(attachmentId: attachId)
         } catch {
-            print(error)
-            outputLabel?.text = "<\(error.localizedDescription)>"
+            print("detachImage(attachId:) failed. \(error.localizedDescription)")
+            throw error
         }
     }
 
-    func fetchDeployment() {
-        MobileMessenger().fetchDeploymentConfig(domain: deployment.domain!, deploymentId: deployment.deploymentId!, logging: true,
-            completionHandler: { deploymentConfig, error in
-            if let error = error {
-                self.outputLabel?.text = "<\(error.localizedDescription)>"
-                return
-            }
-            self.outputLabel?.text = "<\(deploymentConfig?.description() ?? "Unknown deployment config")>"
-        })
+    func fetchDeployment(completion: @escaping (DeploymentConfig?, Error?) -> Void) {
+        MobileMessenger().fetchDeploymentConfig(
+            domain: deployment.domain!,
+            deploymentId: deployment.deploymentId!,
+            logging: true,
+            completionHandler: completion)
     }
 
     func clearConversation() {
--- iosApp/iosAppTests/iosAppTests.swift
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -19,8 +19,6 @@ class iosAppTests: XCTestCase {
             do {
                 let deployment = try Deployment()
                 contentController = TestContentController(deployment: deployment)
-                contentController?.setupSocketListeners()
-                contentController?.setupMessageListener()
             } catch {
                 XCTFail("Failed to initialize Content Controller: \(error.localizedDescription)")
             }
@@ -35,13 +33,13 @@ class iosAppTests: XCTestCase {
         // Setup the session. Send a message to start the conversation.
         // Setup the session. Send a message.
         guard let contentController = contentController else {
-            XCTFail("Faild to setup the content controller.")
+            XCTFail("Failed to setup the content controller.")
             return
         }
 
-        contentController.connect()
-        contentController.configureSession()
-        contentController.sendMessage(text: "Testing from E2E test.")
+        try! contentController.connect()
+        try! contentController.configureSession()
+        try! contentController.sendMessage(text: "Testing from E2E test.")
 
         // Use the public API to answer the new Messenger conversation.
         // Send a message from that agent and make sure we receive it.
@@ -62,24 +60,24 @@ class iosAppTests: XCTestCase {
         for (index, element) in intArray.enumerated() {
             kotlinByteArray.set(index: Int32(index), value: element)
         }
-        contentController.attachImage(kotlinByteArray: kotlinByteArray)
+        try! contentController.attachImage(kotlinByteArray: kotlinByteArray)
         contentController.sendUploadedImage()
 
         // Disconnect the conversation for the agent and disconnect the session.
         ApiHelper.shared.sendConnectOrDisconnect(conversationInfo: conversationInfo, connecting: false, wrapup: true)
-        contentController.disconnect()
+        try! contentController.disconnect()
     }
 
     func testSendAndReceiveMessage() {
         // Setup the session. Send a message.
         guard let contentController = contentController else {
-            XCTFail("Faild to setup the content controller.")
+            XCTFail("Failed to setup the content controller.")
             return
         }
 
-        contentController.connect()
-        contentController.configureSession()
-        contentController.sendMessage(text: "Testing from E2E test.")
+        try! contentController.connect()
+        try! contentController.configureSession()
+        try! contentController.sendMessage(text: "Testing from E2E test.")
 
         // Use the public API to answer the new Messenger conversation.
         // Send a message from that agent and make sure we receive it.
@@ -95,7 +93,7 @@ class iosAppTests: XCTestCase {
 
         // Disconnect the conversation for the agent and disconnect the session.
         ApiHelper.shared.sendConnectOrDisconnect(conversationInfo: conversationInfo, connecting: false, wrapup: true)
-        contentController.disconnect()
+        try! contentController.disconnect()
     }
 
 }
@@ -105,87 +103,28 @@ class TestContentController: MessengerHandler {
     var testExpectation: XCTestExpectation? = nil
     var receivedMessageText: String? = nil
     var receivedDownloadUrl: String? = nil
-
-    override func connect() {
-        testExpectation = XCTestExpectation(description: "Wait for Connection.")
-        super.connect()
-        waitForExpectation()
-    }
-
-    override func configureSession() {
-        testExpectation = XCTestExpectation(description: "Wait for Configuration.")
-        super.configureSession()
-        waitForExpectation()
-    }
-
-    override func disconnect() {
-        testExpectation = XCTestExpectation(description: "Wait for Disconnect.")
-        super.disconnect()
-        waitForExpectation()
-    }
-
-
-    override func sendMessage(text: String) {
-        testExpectation = XCTestExpectation(description: "Wait for message to send.")
-        super.sendMessage(text: text)
-        waitForExpectation()
-        verifyReceivedMessage(expectedMessage: text)
-    }
-
-    override func attachImage(kotlinByteArray: KotlinByteArray) {
-        testExpectation = XCTestExpectation(description: "Wait for image to attach successfully.")
-        super.attachImage(kotlinByteArray: kotlinByteArray)
-        waitForExpectation()
-    }
-
-    func sendUploadedImage() {
-        testExpectation = XCTestExpectation(description: "Wait for the uploaded image url to send.")
-        guard let receivedDownloadUrl = receivedDownloadUrl else {
-            XCTFail("There was no download URL received.")
-            return
-        }
-        super.sendMessage(text: receivedDownloadUrl)
-        waitForExpectation()
-        verifyReceivedMessage(expectedMessage: receivedDownloadUrl)
-    }
-
-    func waitForExpectation() {
-        let result = XCTWaiter().wait(for: [testExpectation!], timeout: 60)
-        XCTAssertEqual(result, .completed, "Expectation never fullfilled: \(testExpectation?.description ?? "No description.")")
-    }
-
-    func verifyReceivedMessage(expectedMessage: String) {
-        print("Checking the received message...")
-        XCTAssertEqual(expectedMessage, receivedMessageText, "The received message: '\(receivedMessageText ?? "")' didn't match what was expected: '\(expectedMessage)'.")
-        receivedMessageText = nil
-    }
-
-    override func setupSocketListeners() {
+    
+    override init(deployment: Deployment) {
+        super.init(deployment: deployment)
+        
         client.stateListener = { [weak self] state in
+            print("State Event: \(state)")
             switch state {
-            case _ as MessagingClientState.Connecting:
-                print("connecting state")
             case _ as MessagingClientState.Connected:
-                print("connected")
                 self?.testExpectation?.fulfill()
-            case let configured as MessagingClientState.Configured:
-                print("Socket <configured>. connected: <\(configured.connected.description)> , newSession: <\(configured.newSession?.description ?? "nill")>")
+            case _ as MessagingClientState.Configured:
                 self?.testExpectation?.fulfill()
-            case let closing as MessagingClientState.Closing:
-                print("Socket <closing>. reason: <\(closing.reason.description)> , code: <\(closing.code.description)>")
-            case let closed as MessagingClientState.Closed:
-                print("Socket <closed>. reason: <\(closed.reason.description)> , code: <\(closed.code.description)>")
+            case _ as MessagingClientState.Closed:
                 self?.testExpectation?.fulfill()
             case let error as MessagingClientState.Error:
                 XCTFail("Socket <error>. code: <\(error.code.description)> , message: <\(error.message ?? "No message")>")
             default:
-                print("Unexpected stateListener state: \(state)")
+                break
             }
+            self?.onStateChange?(state)
         }
-    }
-
-    override func setupMessageListener() {
-        client.messageListener = { [ weak self ] event in
+        
+        client.messageListener = { [weak self] event in
             switch event {
             case let messageInserted as MessageEvent.MessageInserted:
                 print("Message Inserted: <\(messageInserted.message.description)>")
@@ -207,6 +146,63 @@ class TestContentController: MessengerHandler {
             default:
                 print("Unexpected messageListener event: \(event)")
             }
+            self?.onMessageEvent?(event)
         }
+        
     }
+
+    override func connect() throws {
+        testExpectation = XCTestExpectation(description: "Wait for Connection.")
+        try super.connect()
+        waitForExpectation()
+    }
+
+    override func configureSession() throws {
+        testExpectation = XCTestExpectation(description: "Wait for Configuration.")
+        try super.configureSession()
+        waitForExpectation()
+    }
+
+    override func disconnect() throws {
+        testExpectation = XCTestExpectation(description: "Wait for Disconnect.")
+        try super.disconnect()
+        waitForExpectation()
+    }
+
+
+    override func sendMessage(text: String) throws {
+        testExpectation = XCTestExpectation(description: "Wait for message to send.")
+        try super.sendMessage(text: text)
+        waitForExpectation()
+        verifyReceivedMessage(expectedMessage: text)
+    }
+
+    override func attachImage(kotlinByteArray: KotlinByteArray) throws {
+        testExpectation = XCTestExpectation(description: "Wait for image to attach successfully.")
+        try super.attachImage(kotlinByteArray: kotlinByteArray)
+        waitForExpectation()
+    }
+
+    func sendUploadedImage() {
+        testExpectation = XCTestExpectation(description: "Wait for the uploaded image url to send.")
+        guard let receivedDownloadUrl = receivedDownloadUrl else {
+            XCTFail("There was no download URL received.")
+            return
+        }
+        try? super.sendMessage(text: receivedDownloadUrl)
+        waitForExpectation()
+        verifyReceivedMessage(expectedMessage: receivedDownloadUrl)
+    }
+
+    func waitForExpectation() {
+        let result = XCTWaiter().wait(for: [testExpectation!], timeout: 60)
+        XCTAssertEqual(result, .completed, "Expectation never fullfilled: \(testExpectation?.description ?? "No description.")")
+    }
+
+    func verifyReceivedMessage(expectedMessage: String) {
+        print("Checking the received message...")
+        XCTAssertEqual(expectedMessage, receivedMessageText, "The received message: '\(receivedMessageText ?? "")' didn't match what was expected: '\(expectedMessage)'.")
+        receivedMessageText = nil
+    }
+
 }
