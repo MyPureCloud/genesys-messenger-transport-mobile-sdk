945621435e2e1b94c5ba4caa4d89f7dc307d21cd
Brian Dupuis <brian.dupuis@genesys.com>
2022-08-16 13:30:30 -0400

    Lint fixes.

--- transport/src/androidMain/kotlin/com/genesys/cloud/messenger/transport/util/TokenGenerator.kt
+++ transport/src/androidMain/kotlin/com/genesys/cloud/messenger/transport/util/TokenGenerator.kt
@@ -1,6 +1,6 @@
 package com.genesys.cloud.messenger.transport.util
 
-import java.util.*
+import java.util.UUID
 
 actual object TokenGenerator {
     actual fun generate() = UUID.randomUUID().toString()
