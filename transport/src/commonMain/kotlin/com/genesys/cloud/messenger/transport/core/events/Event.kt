904c734e88712492d1f5b8c9021601265bc3270e
Anton Afanasiev <afanasev.anton@gmail.com>
2022-10-14 12:41:48 -0300

    MTSDK-96 Add Autostart event. (#120)
    * MTSDK-96 Add Autostart event.
    
    - Add AutostartRequest model to send to the Shyrka.
    - Map Autostart (Presence) event to ConversationAutostart event.

--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/core/events/Event.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/core/events/Event.kt
@@ -32,4 +32,9 @@ sealed class Event {
         val message: String?,
         val correctiveAction: CorrectiveAction,
     ) : Event()
+
+    /**
+     * This event indicates that the conversation was successfully autostarted.
+     */
+    object ConversationAutostart : Event()
 }
