1d041c72e52af191ee1999e8cfa62dae25b78dc5
aafanasi <Anton.Afanasiev@genesys.com>
2022-11-04 12:54:09 -0300

    MTSDK-15 Handle ConnectionClosedEvent
    - Update KDoc

--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/core/events/Event.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/core/events/Event.kt
@@ -40,7 +40,7 @@ sealed class Event {
 
     /**
      * Sent when the connection is closed due to exceeding the maximum number of simultaneously open sessions.\
-     * Detailed information about Genesys Cloud Web Messaging capabilities is available in the [Developer Center.(https://developer.genesys.cloud).
+     * Detailed information about Genesys Cloud Web Messaging capabilities is available in the [Developer Center](https://developer.genesys.cloud).
      */
     object ConnectionClosed : Event()
 }
