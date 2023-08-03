e5b172b08370e4d1f58e0f39b9ce9eb6190cf9ea
aafanasi <Anton.Afanasiev@genesys.com>
2023-08-03 11:08:46 -0300

    MTSDK-221 Refactor MessagingClientImplTest.kt
    - Break down MessagingClientImpl tests to separate files organized by feature.

--- transport/src/androidTest/kotlin/com/genesys/cloud/messenger/transport/util/Response.kt
+++ transport/src/androidTest/kotlin/com/genesys/cloud/messenger/transport/util/Response.kt
@@ -39,6 +39,10 @@ internal object Response {
         """{"type":"message","class":"LogoutEvent","code":200,"body":{}}"""
     const val unauthorized =
         """{"type": "response","class": "string","code": 401,"body": "User is unauthorized"}"""
+    const val sessionClearedEvent =
+        """{"type":"message","class":"SessionClearedEvent","code":200,"body":{}}"""
+    fun clearConversationForbidden(errorMessage: String = "Presence events Conversation Clear are not supported") =
+        """{"type":"response","class":"string","code":403,"body":"$errorMessage"}"""
 
     fun structuredMessageWithEvents(
         events: String = defaultStructuredEvents,
