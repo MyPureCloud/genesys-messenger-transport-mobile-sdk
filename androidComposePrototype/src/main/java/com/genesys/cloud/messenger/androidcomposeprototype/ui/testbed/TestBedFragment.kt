59991baa509557db2fe3f225c5d6221de1bc1dbc
aafanasi <Anton.Afanasiev@genesys.com>
2023-10-10 11:48:12 -0300

    MTSDK-240 Implement SCP on Android Testbed
    - Replace old(static) attachment selection mechanism with file picker.

--- androidComposePrototype/src/main/java/com/genesys/cloud/messenger/androidcomposeprototype/ui/testbed/TestBedFragment.kt
+++ androidComposePrototype/src/main/java/com/genesys/cloud/messenger/androidcomposeprototype/ui/testbed/TestBedFragment.kt
@@ -1,14 +1,24 @@
 package com.genesys.cloud.messenger.androidcomposeprototype.ui.testbed
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.genesys.cloud.messenger.transport.core.FileAttachmentProfile
+import com.genesys.cloud.messenger.transport.util.DefaultVault
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class TestBedFragment : Fragment() {
 
@@ -20,7 +30,10 @@ class TestBedFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View = ComposeView(requireContext()).apply {
         setViewContent(this)
-        viewModel.init(requireContext()) { url ->
+        viewModel.init(
+            requireContext(),
+            { fileAttachmentProfile -> selectFile(fileAttachmentProfile) })
+        { url ->
             launchCustomTabs(url)
         }
     }
@@ -35,5 +48,50 @@ class TestBedFragment : Fragment() {
         println("Launching chrome custom tab with url: $url")
         val customTabsIntent = CustomTabsIntent.Builder().build()
         customTabsIntent.launchUrl(requireContext(), Uri.parse(url))
+    }
+
+    private fun selectFile(fileAttachmentProfile: FileAttachmentProfile) {
+        val filesWithType =
+            if (fileAttachmentProfile.hasWildCard) arrayOf("*/*") else fileAttachmentProfile.allowedFileTypes.toTypedArray()
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = filesWithType[0]
+            putExtra(Intent.EXTRA_MIME_TYPES, filesWithType)
+        }
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ::handleFileSelectionResult
+        ).launch(intent)
+    }
+
+    private fun handleFileSelectionResult(result: ActivityResult) {
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.data?.let { uri ->
+                viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+                    processFile(uri)
+                }
+            } ?: viewModel.onCancelFileSelection()
+        } else {
+            viewModel.onCancelFileSelection()
+        }
+    }
+
+    private fun processFile(uri: Uri) {
+        try {
+            context?.contentResolver?.openInputStream(uri)?.use {
+                viewModel.onFileSelected(it.readBytes(), uri.getFileName())
+            }
+        } catch (e: Exception) {
+            viewModel.onErrorFilePick(e)
+        }
+    }
+
+    private fun Uri.getFileName(): String {
+        val cursor = DefaultVault.context?.contentResolver?.query(this, null, null, null, null)
+        return cursor?.use {
+            val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            it.moveToFirst()
+            it.getString(nameIndex)
+        } ?: "unknown"
     }
 }
