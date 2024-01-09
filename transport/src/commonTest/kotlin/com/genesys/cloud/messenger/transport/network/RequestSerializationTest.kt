be8df2ec173d0e94a8bce5a2a4b7f44b49adc210
aafanasi <Anton.Afanasiev@genesys.com>
2024-01-09 10:17:42 -0400

    MTSDK-305 Increase unit test coverage
    - Add/update unit tests

--- transport/src/commonTest/kotlin/com/genesys/cloud/messenger/transport/network/RequestSerializationTest.kt
+++ transport/src/commonTest/kotlin/com/genesys/cloud/messenger/transport/network/RequestSerializationTest.kt
@@ -9,14 +9,20 @@ import assertk.assertions.isTrue
 import com.genesys.cloud.messenger.transport.shyrka.WebMessagingJson
 import com.genesys.cloud.messenger.transport.shyrka.receive.PresenceEvent
 import com.genesys.cloud.messenger.transport.shyrka.receive.StructuredMessageEvent
+import com.genesys.cloud.messenger.transport.shyrka.receive.TypingEvent
 import com.genesys.cloud.messenger.transport.shyrka.send.AutoStartRequest
 import com.genesys.cloud.messenger.transport.shyrka.send.Channel
 import com.genesys.cloud.messenger.transport.shyrka.send.ClearConversationRequest
 import com.genesys.cloud.messenger.transport.shyrka.send.CloseSessionRequest
 import com.genesys.cloud.messenger.transport.shyrka.send.ConfigureAuthenticatedSessionRequest
+import com.genesys.cloud.messenger.transport.shyrka.send.ConfigureSessionRequest
+import com.genesys.cloud.messenger.transport.shyrka.send.DeleteAttachmentRequest
+import com.genesys.cloud.messenger.transport.shyrka.send.EchoRequest
 import com.genesys.cloud.messenger.transport.shyrka.send.EventMessage
 import com.genesys.cloud.messenger.transport.shyrka.send.GetAttachmentRequest
+import com.genesys.cloud.messenger.transport.shyrka.send.HealthCheckID
 import com.genesys.cloud.messenger.transport.shyrka.send.RequestAction
+import com.genesys.cloud.messenger.transport.shyrka.send.TextMessage
 import com.genesys.cloud.messenger.transport.utility.AttachmentValues
 import com.genesys.cloud.messenger.transport.utility.AuthTest
 import com.genesys.cloud.messenger.transport.utility.TestValues
@@ -126,10 +132,17 @@ class RequestSerializationTest {
 
         val encodedString = WebMessagingJson.json.encodeToString(expectedRequest)
         val encodedDataString = WebMessagingJson.json.encodeToString(expectedData)
-        val decoded = WebMessagingJson.json.decodeFromString<ConfigureAuthenticatedSessionRequest>(expectedJson)
-        val decodedData = WebMessagingJson.json.decodeFromString<ConfigureAuthenticatedSessionRequest.Data>(expectedDataJson)
+        val decoded = WebMessagingJson.json.decodeFromString<ConfigureAuthenticatedSessionRequest>(
+            expectedJson
+        )
+        val decodedData =
+            WebMessagingJson.json.decodeFromString<ConfigureAuthenticatedSessionRequest.Data>(
+                expectedDataJson
+            )
 
-        assertThat(encodedString, "encoded ConfigureAuthenticatedSessionRequest").isEqualTo(expectedJson)
+        assertThat(encodedString, "encoded ConfigureAuthenticatedSessionRequest").isEqualTo(
+            expectedJson
+        )
         assertThat(encodedDataString, "encoded Data").isEqualTo(expectedDataJson)
         decoded.run {
             assertThat(action).isEqualTo(RequestAction.CONFIGURE_AUTHENTICATED_SESSION.value)
@@ -140,6 +153,98 @@ class RequestSerializationTest {
         }
         assertThat(decodedData).isEqualTo(expectedData)
         assertThat(decodedData.code).isEqualTo(AuthTest.JwtToken)
+    }
+
+    @Test
+    fun `validate ConfigureSessionRequest serialization`() {
+        val expectedRequest = ConfigureSessionRequest(
+            token = TestValues.Token,
+            deploymentId = TestValues.DeploymentId,
+            startNew = true,
+        )
+        val expectedJson =
+            """{"token":"<token>","deploymentId":"deploymentId","startNew":true,"action":"configureSession"}"""
+
+        val encodedString = WebMessagingJson.json.encodeToString(expectedRequest)
+        val decoded = WebMessagingJson.json.decodeFromString<ConfigureSessionRequest>(expectedJson)
+
+        assertThat(encodedString, "encoded ConfigureSessionRequest").isEqualTo(expectedJson)
+        decoded.run {
+            assertThat(action).isEqualTo(RequestAction.CONFIGURE_SESSION.value)
+            assertThat(token).isEqualTo(TestValues.Token)
+            assertThat(deploymentId).isEqualTo(TestValues.DeploymentId)
+            assertThat(startNew).isTrue()
+            assertThat(journeyContext).isNull()
+        }
+    }
+
+    @Test
+    fun `validate DeleteAttachmentRequest serialization`() {
+        val expectedRequest = DeleteAttachmentRequest(
+            token = TestValues.Token,
+            attachmentId = AttachmentValues.Id
+        )
+        val expectedJson =
+            """{"token":"<token>","attachmentId":"test_attachment_id","action":"deleteAttachment"}"""
+
+        val encodedString = WebMessagingJson.json.encodeToString(expectedRequest)
+        val decoded = WebMessagingJson.json.decodeFromString<DeleteAttachmentRequest>(expectedJson)
+
+        assertThat(encodedString, "encoded DeleteAttachmentRequest").isEqualTo(expectedJson)
+        decoded.run {
+            assertThat(action).isEqualTo(RequestAction.DELETE_ATTACHMENT.value)
+            assertThat(token).isEqualTo(TestValues.Token)
+            assertThat(attachmentId).isEqualTo(AttachmentValues.Id)
+        }
+    }
+
+    @Test
+    fun `validate EchoRequest serialization`() {
+        val expectedTextMessage = TextMessage("ping", mapOf("customMessageId" to HealthCheckID))
+        val expectedRequest = EchoRequest(
+            token = TestValues.Token,
+        )
+        val expectedJson =
+            """{"token":"<token>","action":"echo","message":{"text":"ping","metadata":{"customMessageId":"SGVhbHRoQ2hlY2tNZXNzYWdlSWQ="},"type":"Text"}}"""
+
+        val encodedString = WebMessagingJson.json.encodeToString(expectedRequest)
+        val decoded = WebMessagingJson.json.decodeFromString<EchoRequest>(expectedJson)
+
+        assertThat(encodedString, "encoded EchoRequest").isEqualTo(expectedJson)
+        decoded.run {
+            assertThat(action).isEqualTo(RequestAction.ECHO_MESSAGE.value)
+            assertThat(token).isEqualTo(TestValues.Token)
+            assertThat(message).isEqualTo(expectedTextMessage)
+            message.run {
+                assertThat(text).isEqualTo(expectedTextMessage.text)
+                assertThat(metadata?.get("customMessageId")).isEqualTo(HealthCheckID)
+            }
+        }
+    }
+
+    @Test
+    fun `validate EventMessage serialization`() {
+        val expectedEvent = TypingEvent(
+            eventType = StructuredMessageEvent.Type.Typing,
+            typing = TypingEvent.Typing(type = "typing", duration = 100)
+        )
+        val expectedEvents = listOf(expectedEvent)
+        val expectedRequest = EventMessage(events = expectedEvents)
+        val expectedJson =
+            """{"events":[{"eventType":"Typing","typing":{"type":"typing","duration":100}}],"type":"Event"}"""
+
+        val encodedString = WebMessagingJson.json.encodeToString(expectedRequest)
+        val decoded = WebMessagingJson.json.decodeFromString<EventMessage>(expectedJson)
+
+        assertThat(encodedString, "encoded EventMessage").isEqualTo(expectedJson)
+        decoded.run {
+            assertThat(this).isEqualTo(expectedRequest)
+            assertThat(channel).isNull()
+            assertThat(events).containsExactly(*expectedEvents.toTypedArray())
+                assertThat((events[0] as TypingEvent).eventType).isEqualTo(StructuredMessageEvent.Type.Typing)
+                assertThat((events[0] as TypingEvent).typing.type).isEqualTo("typing")
+                assertThat((events[0] as TypingEvent).typing.duration).isEqualTo(100)
+        }
     }
 
     @Test
