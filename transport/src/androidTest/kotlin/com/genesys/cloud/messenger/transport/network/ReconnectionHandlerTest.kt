1152c584d7aa84bb6b5a304e3021d94f18acfed7
aafanasi <Anton.Afanasiev@genesys.com>
2024-01-10 14:45:04 -0400

    MTSDK-305 Increase unit test coverage
    - Add/update unit tests

--- transport/src/androidTest/kotlin/com/genesys/cloud/messenger/transport/network/ReconnectionHandlerTest.kt
+++ transport/src/androidTest/kotlin/com/genesys/cloud/messenger/transport/network/ReconnectionHandlerTest.kt
@@ -1,15 +1,22 @@
 package com.genesys.cloud.messenger.transport.network
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import com.genesys.cloud.messenger.transport.util.logs.Log
+import com.genesys.cloud.messenger.transport.utility.LogMessages
 import com.genesys.cloud.messenger.transport.utility.TestValues
 import io.mockk.MockKAnnotations
+import io.mockk.coVerify
 import io.mockk.mockk
+import io.mockk.spyk
 import io.mockk.verify
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
@@ -19,7 +26,7 @@ import org.junit.Test
 class ReconnectionHandlerTest {
     internal val mockLogger: Log = mockk(relaxed = true)
     internal val logSlot = mutableListOf<() -> String>()
-    private val mockReconnectFunction: () -> Unit = mockk(relaxed = true)
+    private val mockReconnectFunction: () -> Unit = spyk()
     private val dispatcher: CoroutineDispatcher = Dispatchers.Unconfined
 
     private var subject = ReconnectionHandlerImpl(TestValues.ReconnectionTimeout, mockLogger)
@@ -37,23 +44,31 @@ class ReconnectionHandlerTest {
         Dispatchers.resetMain()
     }
 
-//    @Test
-//    fun `when reconnect() and there are reconnection attempts left`(){
-//        subject.reconnect { mockReconnectFunction  }
-//
-//        verify {
-//            mockReconnectFunction.invoke()
-//        }
-//        assertThat(subject.shouldReconnect).isTrue()
-//    }
+    @Test
+    fun `when reconnect() and there are reconnection attempts left`() {
+        runBlocking {
+            subject.reconnect(mockReconnectFunction)
+            delay(5000)
+        }
+
+        coVerify {
+            mockLogger.i(capture(logSlot))
+            mockReconnectFunction()
+        }
+        assertThat(subject.shouldReconnect).isTrue()
+        assertThat(logSlot[0].invoke()).isEqualTo(LogMessages.Reconnecting)
+    }
 
     @Test
     fun `when reconnect() and there is NO reconnection attempts left`() {
         subject = ReconnectionHandlerImpl(TestValues.NoReconnectionAttempts, mockLogger)
 
-        subject.reconnect { mockReconnectFunction }
+        subject.reconnect(mockReconnectFunction)
 
-        verify(exactly = 0) { mockReconnectFunction.invoke() }
+        verify(exactly = 0) {
+            mockReconnectFunction.invoke()
+            mockLogger.i(any())
+        }
         assertThat(subject.shouldReconnect).isFalse()
     }
 }
--- transport/src/commonTest/kotlin/com/genesys/cloud/messenger/transport/utility/TestValues.kt
+++ transport/src/commonTest/kotlin/com/genesys/cloud/messenger/transport/utility/TestValues.kt
@@ -83,6 +83,7 @@ object LogMessages {
     internal const val Attach = "attach(fileName = test.png)"
     internal const val Detach = "detach(attachmentId = 88888888-8888-8888-8888-888888888888)"
     internal const val WillSendMessage = "Will send message"
+    internal const val Reconnecting = "Trying to reconnect. Attempt number: 1 out of 1000"
     internal const val ForceClose = "Force close web socket."
     internal const val CloseSession = "closeSession()"
     internal const val Disconnect = "disconnect()"
