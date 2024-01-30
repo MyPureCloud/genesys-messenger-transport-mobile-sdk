f329790d6c0082f36867c73a9ddbdbe5bedee704
aafanasi <Anton.Afanasiev@genesys.com>
2024-01-30 15:31:12 -0400

    MTSDK-198 Implement QuickReply on iOS Testbed app.
    - Store quick replies as map when received.
    - Implement sendQuickReply command.
    - Add sendQuickReply command to MessengerInteractor.swift
    - Add print of Event.ConversationDisconnect that was missing on iOS.
    - Align print of quick replies received message between iOS and Android.

--- androidComposePrototype/src/main/java/com/genesys/cloud/messenger/androidcomposeprototype/ui/testbed/TestBedViewModel.kt
+++ androidComposePrototype/src/main/java/com/genesys/cloud/messenger/androidcomposeprototype/ui/testbed/TestBedViewModel.kt
@@ -370,7 +370,7 @@ class TestBedViewModel : ViewModel(), CoroutineScope {
             is MessageEvent.QuickReplyReceived -> event.message.run {
                 quickRepliesMap.clear()
                 quickRepliesMap.putAll(quickReplies.associateBy { it.text })
-                "QuickReplyReceived: text: ${this.text} | quick reply options: ${this.quickReplies}"
+                "QuickReplyReceived: text: $text | quick reply options: $quickReplies"
             }
 
             else -> event.toString()
