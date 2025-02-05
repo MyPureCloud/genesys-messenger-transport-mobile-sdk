03b35efab6a0e3019e7f35da29b4c5f08cd2afc4
Anton Afanasiev <afanasev.anton@gmail.com>
2025-02-05 11:27:32 -0400

    MTSDK-524 Public api to sync device token (#307)
    * MTSDK-524 Public api to sync device token
    
    - Add utility function in MessengerTransportSDK that  creates and return PushService.
    - Add PushService.kt interface as public api.
    - Add synchronize() function to PushService
    - Add PushServiceImpl.kt to handle push related functionality.
    - Add PushProvider.kt to represent possible push notification providers on mobile.
    - Add KDoc

---
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/push/PushService.kt
@@ -0,0 +1,18 @@
+package com.genesys.cloud.messenger.transport.push
+
+/**
+ * Service interface for push notification operations.
+ */
+interface PushService {
+
+    /**
+     * Synchronizes the device token with the specified notification provider.
+     *
+     * @param deviceToken The device token for push notifications.
+     * @param pushProvider The type of notification provider to use.
+     *
+     * @throws Exception if synchronization fails.
+     */
+    @Throws(Exception::class)
+    suspend fun synchronize(deviceToken: String, pushProvider: PushProvider)
+}
---
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/push/PushServiceImpl.kt
@@ -0,0 +1,9 @@
+package com.genesys.cloud.messenger.transport.push
+
+internal class PushServiceImpl : PushService {
+
+    @Throws(Exception::class)
+    override suspend fun synchronize(deviceToken: String, pushProvider: PushProvider) {
+        // TODO("Not yet implemented")
+    }
+}
