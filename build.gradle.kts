1d9c3a7033c768dd7d4a967e916c8ae4e897b438
aafanasi <afanasev.anton@gmail.com>
2025-03-21 13:02:33 -0300

    release/2.9.0-rc2
    - Version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 }
 
 // CocoaPods requires the podspec to have a `version`.
-val buildVersion = "2.9.0-rc1"
+val buildVersion = "2.9.0.2"
 val snapshot = System.getenv("SNAPSHOT_BUILD") ?: ""
 version = "${buildVersion}${snapshot}"
 group = "cloud.genesys"
