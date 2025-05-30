1908c802c8fe70d476682d4597c9eb093787a580
aafanasi <afanasev.anton@gmail.com>
2025-05-30 11:50:27 -0300

    MTSDK-423-Push-Notification - fix merging conflicts

--- iosApp/iosApp/MessengerInteractor.swift
+++ iosApp/iosApp/MessengerInteractor.swift
@@ -30,7 +30,7 @@ final class MessengerInteractor {
                                            reconnectionTimeoutInSeconds: reconnectTimeout,
                                            autoRefreshTokenWhenExpired: true,
                                            encryptedVault: true)
-        self.tokenVault = DefaultVault(keys: Vault.Keys(vaultKey: "com.genesys.cloud.messenger", tokenKey: "token", authRefreshTokenKey: "auth_refresh_token", wasAuthenticated: "wasAuthenticated"))
+        self.tokenVault = DefaultVault(keys: Vault.Keys(vaultKey: "com.genesys.cloud.messenger", tokenKey: "token", authRefreshTokenKey: "auth_refresh_token", wasAuthenticated: "wasAuthenticated", pushConfigKey: "pushConfig"))
         self.messengerTransport = MessengerTransportSDK(configuration: self.configuration, vault: self.tokenVault)
         self.messagingClient = self.messengerTransport.createMessagingClient()
         
