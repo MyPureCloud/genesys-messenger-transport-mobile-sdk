0ed1153fed11a7ba50785b95e91ae1df614667ec
aafanasi <afanasev.anton@gmail.com>
2025-08-12 21:44:31 -0300

    MTSDK-865 Implement retry mechanism on fetchAuthJWT request
    - Add retryable mechanism to retry fetchAuthJwt request upon common server errors.

--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/network/HttpClientProvider.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/network/HttpClientProvider.kt
@@ -5,13 +5,25 @@ import com.genesys.cloud.messenger.transport.util.logs.Log
 import com.genesys.cloud.messenger.transport.util.logs.LogTag
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpCallValidator
+import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.retry
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 
 private const val TIMEOUT_IN_MS = 30000L
+private const val MAX_RETRIES_ON_INTERNAL_SERVER_ERRORS = 3
+private const val DELAY_BETWEEN_RETRIES_IN_MILLISECONDS = 5000L
+private val RETRYABLE_STATUS_CODES = setOf(
+    HttpStatusCode.InternalServerError,
+    HttpStatusCode.BadGateway,
+    HttpStatusCode.ServiceUnavailable,
+    HttpStatusCode.GatewayTimeout,
+)
 
 internal fun defaultHttpClient(logging: Boolean = false): HttpClient = HttpClient {
     if (logging) {
@@ -27,5 +39,14 @@ internal fun defaultHttpClient(logging: Boolean = false): HttpClient = HttpClien
     install(HttpTimeout) {
         socketTimeoutMillis = TIMEOUT_IN_MS
         connectTimeoutMillis = TIMEOUT_IN_MS
+    }
+    install(HttpRequestRetry)
+}
+
+internal fun HttpRequestBuilder.retryOnServerErrors() {
+    retry {
+        maxRetries = MAX_RETRIES_ON_INTERNAL_SERVER_ERRORS
+        constantDelay(DELAY_BETWEEN_RETRIES_IN_MILLISECONDS)
+        retryIf { _, response -> response.status in RETRYABLE_STATUS_CODES }
     }
 }
