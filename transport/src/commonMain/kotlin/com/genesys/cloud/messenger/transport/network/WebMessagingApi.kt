e6fe03ab8bdb7db1320e2f64ee1eeea7f52e6057
Anton Afanasiev <afanasev.anton@gmail.com>
2025-08-13 10:33:01 -0500

    Task/mtsdk 865 implement retry mechanism on fetch auth jwt request (#391)
    * MTSDK-865 Implement retry mechanism on fetchAuthJWT request
    
    - Add retryable mechanism to retry fetchAuthJwt request upon common server errors.

--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/network/WebMessagingApi.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/network/WebMessagingApi.kt
@@ -106,6 +106,7 @@ internal class WebMessagingApi(
         val response = client.post(configuration.jwtAuthUrl.toString()) {
             header("content-type", ContentType.Application.Json)
             setBody(requestBody)
+            retryOnServerErrors()
         }
         if (response.status.isSuccess()) {
             Result.Success(response.body())
