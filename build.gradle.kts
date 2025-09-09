3d329136ab3a652258307ff653356cce589e6896
Janos Kernacs <janos.kernacs@genesys.com>
2025-09-09 09:55:13 +0200

    release/2.9.3-rc3

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 // CocoaPods requires the podspec to have a `version`
-val buildVersion = "2.9.3-rc2"
+val buildVersion = "2.9.3-rc3"
 val snapshot = System.getenv("SNAPSHOT_BUILD") ?: ""
 version = "${buildVersion}${snapshot}"
 group = "cloud.genesys"
