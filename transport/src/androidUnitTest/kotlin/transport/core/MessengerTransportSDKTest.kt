7aa4faaec1c4eb3741361cdee18327751696e539
aafanasi <afanasev.anton@gmail.com>
2025-10-02 12:43:51 -0300

    Apply test naming conventions
    - Add unit test to cover MessagingClient creation

--- transport/src/androidUnitTest/kotlin/transport/core/MessengerTransportSDKTest.kt
+++ transport/src/androidUnitTest/kotlin/transport/core/MessengerTransportSDKTest.kt
@@ -3,6 +3,7 @@ package transport.core
 import android.content.Context
 import assertk.assertThat
 import assertk.assertions.isInstanceOf
+import com.genesys.cloud.messenger.transport.core.MessagingClient
 import com.genesys.cloud.messenger.transport.core.MessengerTransportSDK
 import com.genesys.cloud.messenger.transport.util.DefaultVault
 import com.genesys.cloud.messenger.transport.util.EncryptedVault
@@ -16,7 +17,6 @@ import org.junit.Test
 
 class MessengerTransportSDKTest {
 
-    // Mocks
     private val mockContext = mockk<Context>(relaxed = true)
     private val mockApplicationContext = mockk<Context>(relaxed = true)
 
@@ -35,14 +35,14 @@ class MessengerTransportSDKTest {
     }
 
     @Test
-    fun `should create DefaultVault when encryptedVault is false`() {
+    fun `when MessengerTransportSDK() is created with encryptedVault false`() {
         val subject = MessengerTransportSDK(TestValues.configuration)
 
         assertThat(subject.vault).isInstanceOf(DefaultVault::class.java)
     }
 
     @Test
-    fun `should create EncryptedVault when encryptedVault is true`() {
+    fun `when MessengerTransportSDK() is created with encryptedVault true`() {
         val configuration = TestValues.configuration.copy(encryptedVault = true)
 
         val subject = MessengerTransportSDK(configuration)
@@ -51,12 +51,21 @@ class MessengerTransportSDKTest {
     }
 
     @Test
-    fun `should use provided vault regardless of encryptedVault setting`() {
+    fun `when MessengerTransportSDK() is created with provided vault`() {
         val configuration = TestValues.configuration.copy(encryptedVault = true)
         val fakeVault = FakeVault(TestValues.vaultKeys)
 
         val subject = MessengerTransportSDK(configuration, fakeVault)
 
         assertThat(subject.vault).isInstanceOf(FakeVault::class.java)
+    }
+
+    @Test
+    fun `when createMessagingClient() is called`() {
+        val subject = MessengerTransportSDK(TestValues.configuration)
+
+        val result = subject.createMessagingClient()
+
+        assertThat(result).isInstanceOf(MessagingClient::class.java)
     }
 }
