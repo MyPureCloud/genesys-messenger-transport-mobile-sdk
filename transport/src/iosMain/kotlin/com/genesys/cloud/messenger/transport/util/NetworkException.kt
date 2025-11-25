62bb5b0ff5d76eb5ee87651706b988c01fe67bd4
Yarin Sason <yarin.sason@genesys.com>
2025-11-25 10:28:03 +0200

    MTSDK-961 fix network exception matching for ios targets

--- transport/src/iosMain/kotlin/com/genesys/cloud/messenger/transport/util/NetworkException.kt
+++ transport/src/iosMain/kotlin/com/genesys/cloud/messenger/transport/util/NetworkException.kt
@@ -1,27 +1,7 @@
 package com.genesys.cloud.messenger.transport.util
 
-import platform.Foundation.NSError
-import platform.Foundation.NSURLErrorDomain
+import io.ktor.client.engine.darwin.DarwinHttpRequestException
 
 internal actual fun Exception.isNetworkException(): Boolean {
-    val nsError =
-        when {
-            this is NSError -> this
-            this.cause is NSError -> this.cause as NSError
-            else -> null
-        }
-
-    if (nsError != null && nsError.domain == NSURLErrorDomain) {
-        return when (nsError.code) {
-            -1009L, // NSURLErrorNotConnectedToInternet
-            -1004L, // NSURLErrorCannotConnectToHost
-            -1003L, // NSURLErrorCannotFindHost
-            -1005L, // NSURLErrorNetworkConnectionLost
-            -1001L // NSURLErrorTimedOut
-            -> true
-            else -> false
-        }
-    }
-
-    return false
+    return this is DarwinHttpRequestException
 }
