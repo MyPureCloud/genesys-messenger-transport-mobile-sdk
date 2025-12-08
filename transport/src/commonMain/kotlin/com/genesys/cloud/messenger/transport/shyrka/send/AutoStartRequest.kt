5675e780434b79fe8afac00a1df779703fc6c39a
Yarin Sason <yarin.sason@genesys.com>
2025-12-08 14:29:36 +0200

    MTSDK-313 add new field for tracingId

--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/shyrka/send/AutoStartRequest.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/shyrka/send/AutoStartRequest.kt
@@ -2,6 +2,7 @@ package com.genesys.cloud.messenger.transport.shyrka.send
 
 import com.genesys.cloud.messenger.transport.shyrka.receive.PresenceEvent
 import com.genesys.cloud.messenger.transport.shyrka.receive.StructuredMessageEvent
+import com.genesys.cloud.messenger.transport.util.Platform
 import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
@@ -11,6 +12,7 @@ internal data class AutoStartRequest(
     override val token: String,
     @Transient
     private val channel: Channel? = null,
+    override val tracingId: String = Platform().randomUUID(),
 ) : WebMessagingRequest {
     @Required
     override val action: String = RequestAction.ON_MESSAGE.value
--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/shyrka/send/EchoRequest.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/shyrka/send/EchoRequest.kt
@@ -1,5 +1,6 @@
 package com.genesys.cloud.messenger.transport.shyrka.send
 
+import com.genesys.cloud.messenger.transport.util.Platform
 import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
 
@@ -7,7 +8,8 @@ internal const val HealthCheckID = "SGVhbHRoQ2hlY2tNZXNzYWdlSWQ="
 
 @Serializable
 internal data class EchoRequest(
-    override val token: String
+    override val token: String,
+    override val tracingId: String = Platform().randomUUID(),
 ) : WebMessagingRequest {
     @Required override val action: String = RequestAction.ECHO_MESSAGE.value
     @Required val message: TextMessage = TextMessage("ping", metadata = mapOf("customMessageId" to HealthCheckID))
