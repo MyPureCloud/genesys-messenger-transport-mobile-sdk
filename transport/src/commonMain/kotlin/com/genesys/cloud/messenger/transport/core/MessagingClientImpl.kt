6887d7990ee2ca41a2a5ee1a51131979acf5470a
Janos Kernacs <janos.kernacs@genesys.com>
2025-12-16 15:37:19 +0100

    MTSDK-1094 Adjust error handling when id token is expired

--- transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/core/MessagingClientImpl.kt
+++ transport/src/commonMain/kotlin/com/genesys/cloud/messenger/transport/core/MessagingClientImpl.kt
@@ -508,6 +508,17 @@ internal class MessagingClientImpl(
             is ErrorCode.ServerResponseError,
             is ErrorCode.RedirectResponseError,
             -> {
+                if ((code as? ErrorCode.ClientResponseError)?.value == 403) {
+                    message?.run {
+                        if (startsWith("Session authentication failed", true)) {
+                            eventHandler.onEvent(Event.AuthorizationRequired)
+                            return
+                        } else if (startsWith("Try to authenticate again", true)) {
+                            eventHandler.onEvent(Event.AuthorizationRequired)
+                            return
+                        }
+                    }
+                }
                 if (stateMachine.isConnected() || stateMachine.isReconnecting() || isStartingANewSession) {
                     handleConfigureSessionErrorResponse(code, message)
                 } else {
