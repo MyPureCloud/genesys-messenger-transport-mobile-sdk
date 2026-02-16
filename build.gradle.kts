428886d7d92506380b1639f91fd69d9e3d694c0f
Janos Kernacs <janos.kernacs@genesys.com>
2026-02-16 18:12:47 +0100

    MTSDK-751 Dependency updates - Kotlinter and re-apply rules
    - Update kotlinter version 5.0.2 -> 5.4.2
    
    # Conflicts:
    #	androidComposePrototype/src/main/java/com/genesys/cloud/messenger/androidcomposeprototype/ui/testbed/TestBedViewModel.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.kotlinVersion}")
         classpath("com.android.tools.build:gradle:${Deps.agp}")
-        classpath("org.jmailen.gradle:kotlinter-gradle:${Deps.kotlinterVersion}")
         classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:${Deps.buildKonfig}")
         classpath("com.google.gms:google-services:${Deps.googleServices}")
     }
--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -9,7 +9,6 @@ object Deps {
     private const val mockkVersion = "1.14.5"
     private const val okhttpVersion = "4.12.0"
     const val kotlinVersion = "2.3.10"
-    const val kotlinterVersion = "5.0.2"
     const val composeVersion = "1.5.10"
     const val fragmentVersion = "1.3.5"
     const val activityComposeVersion = "1.8.2"
